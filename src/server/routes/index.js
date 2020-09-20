1b68f23945410d7c8a179f9320c51c36eab84374
Lucas Santos <s.cardosolucas@gmail.com>
2020-09-20 17:26:00 -0300

    Adjust get companies by firebase.

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,12 +1,11 @@
 const express = require('express');
 var bodyParser = require('body-parser')
 const companies = require('../../mock/companies.js')
-const firebase = require("firebase/app");
-require('firebase/auth');
-require('firebase/database');
 
 const router = express.Router()
 const sendComplaintController = require('../../controllers/sendComplaintController');
+const fireBaseController = require('../../controllers/fireBaseController');
+
 const { v4: uuidv4, stringify } = require('uuid');
 
 const jsonParser = bodyParser.json()
@@ -24,24 +23,18 @@ router.get('/companies/:id', (req, res) => {
     res.json(company);
 })
 
-router.get('/empresas', (req, res, next) => {
-    const firebaseConfig = {
-        apiKey: "AIzaSyCplKWuqp-n9blxmh4f1iH0HJTs90LOZgc",
-        authDomain: "hackathon-zenvia.firebaseapp.com",
-        databaseURL: "https://hackathon-zenvia.firebaseio.com",
-        projectId: "hackathon-zenvia",
-        storageBucket: "hackathon-zenvia.appspot.com",
-        messagingSenderId: "994976729405",
-        appId: "1:994976729405:web:9d3beed8668e1cb4eb1d54"
-    };
-    if (!firebase.apps.length) {
-        firebase.initializeApp(firebaseConfig);
+router.get('/empresas', async (req, res, next) => {
+
+    const companies = await fireBaseController.getCompanies(req, res);
+    if (companies.length > 0) {
+        res.status(200).json(companies);
+    } else {
+        const body = {
+            'message': 'Não constam empresas cadastradas',
+        }
+        res.status(500).send(body);
     }
-    
-    firebase.database().ref().once('value').then(function (snapshot) {
-        const values = snapshot.val()
-        res.json(values);
-    });;
+
 
 })
 
